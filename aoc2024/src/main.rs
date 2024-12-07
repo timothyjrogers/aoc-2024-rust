c8e5719bb1331ab7a7f4a4e081c4b3994b25bef3
Tim Rogers <rogers.timothy.john@gmail.com>
2024-12-07 16:02:53 -0600

    Day 7 solved (unoptimized)

--- aoc2024/src/main.rs
+++ aoc2024/src/main.rs
@@ -1,27 +1,50 @@
+use std::env;
+
 fn main() {
-    day1::solve();
-    day2::solve();
-    day3::solve();
-    day4::solve();
-    day5::solve();
-    day6::solve();
-    //day7::solve();
-    //day8::solve();
-    //day9::solve();
-    //day10::solve();
-    //day11::solve();
-    //day12::solve();
-    //day13::solve();
-    //day14::solve();
-    //day15::solve();
-    //day16::solve();
-    //day17::solve();
-    //day18::solve();
-    //day19::solve();
-    //day20::solve();
-    //day21::solve();
-    //day22::solve();
-    //day23::solve();
-    //day24::solve();
-    //day25::solve();
+    let args: Vec<String> = env::args().collect();
+    let mut day: i32 = -1;
+    if args.len() > 1 {
+        day = args[1].parse::<i32>().unwrap();
+    }
+    if day == -1 {
+        day1::solve();
+        day2::solve();
+        day3::solve();
+        day4::solve();
+        day5::solve();
+        day6::solve();
+        day7::solve();
+        //day8::solve();
+        //day9::solve();
+        //day10::solve();
+        //day11::solve();
+        //day12::solve();
+        //day13::solve();
+        //day14::solve();
+        //day15::solve();
+        //day16::solve();
+        //day17::solve();
+        //day18::solve();
+        //day19::solve();
+        //day20::solve();
+        //day21::solve();
+        //day22::solve();
+        //day23::solve();
+        //day24::solve();
+        //day25::solve();
+    } else if day == 1 {
+        day1::solve();
+    } else if day == 2 {
+        day2::solve();
+    } else if day == 3 {
+        day3::solve();
+    } else if day == 4 {
+        day4::solve();
+    } else if day == 5 {
+        day5::solve();
+    } else if day == 6 {
+        day6::solve();
+    } else if day == 7 {
+        day7::solve();
+    }
 }
---
+++ day7/src/lib.rs
@@ -0,0 +1,87 @@
+use std::collections::VecDeque;
+
+const INPUT_FILE: &str = "input7.txt";
+
+pub fn solve() {
+    let raw_input: Vec<String> = aoc_utils::input::load_input_lines(INPUT_FILE);
+    let mut cases: Vec<(u64, VecDeque<u64>)> = Vec::new();
+    for line in raw_input {
+        let parts: Vec<&str> = line.split(": ").collect();
+        let target = parts.get(0).unwrap().parse::<u64>().unwrap();
+        let values: VecDeque<u64> = parts.get(1).unwrap().split(" ").map(|s| s.parse::<u64>().unwrap()).collect();
+        cases.push((target, values));
+    }
+    solve_part_one(cases.clone());
+    solve_part_two(cases.clone());
+}
+
+fn solve_part_one(cases: Vec<(u64, VecDeque<u64>)>) {
+    let mut solution = 0;
+    for case in cases {
+        let target = case.0;
+        let values = case.1;
+        if process_equation(values, target) {
+            solution += target;
+        }
+    }
+    println!("Day 7 Part 1: {}", solution);
+}
+
+fn solve_part_two(cases: Vec<(u64, VecDeque<u64>)>) {
+    let mut solution = 0;
+    for case in cases {
+        let target = case.0;
+        let values = case.1;
+        if process_equation_with_pipes(values, target) {
+            solution += target;
+        }
+    }
+    println!("Day 7 Part 2: {}", solution);
+}
+
+fn process_equation(values: VecDeque<u64>, target: u64) -> bool {
+    let mut values = values.clone();
+    if values.len() == 1 {
+        let result = values.pop_front().unwrap();
+        return result == target
+    }
+    let current = values.pop_front().unwrap();
+    let next = values.pop_front().unwrap();
+    let mut add = values.clone();
+    add.push_front(current + next);
+    let mut mul = values.clone();
+    mul.push_front(current * next);
+    if process_equation(add, target) || process_equation(mul, target)  {
+        return true;
+    }
+    return false;
+}
+
+fn process_equation_with_pipes(values: VecDeque<u64>, target: u64) -> bool {
+    let mut values = values.clone();
+    if values.len() == 1 {
+        let result = values.pop_front().unwrap();
+        return result == target
+    }
+    let current = values.pop_front().unwrap();
+    let next = values.pop_front().unwrap();
+    let mut add = values.clone();
+    add.push_front(current + next);
+    let mut mul = values.clone();
+    mul.push_front(current * next);
+    let mut pipes = values.clone();
+    let mut current_string: String = current.to_string();
+    let next_string: String = next.to_string();
+    current_string.push_str(next_string.as_str());
+    pipes.push_front(current_string.parse::<u64>().unwrap());
+    if process_equation_with_pipes(add, target) || process_equation_with_pipes(mul, target) || process_equation_with_pipes(pipes, target)  {
+        return true;
+    }
+    return false;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+}
